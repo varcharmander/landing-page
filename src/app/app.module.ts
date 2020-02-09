e1a9ef89781bb0d8725ee0da3c1231936ce8f601
LuisReinoso <lomejordejr@gmail.com>
2020-02-09 12:40:43 -0500

    feat: Add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScullyLibModule } from '@scullyio/ng-lib';
 import { NameComponent } from './name/name.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      NameComponent
+      NameComponent,
+      HeaderComponent
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
-      ScullyLibModule
+      ScullyLibModule,
+      NgbModule
    ],
    providers: [],
    bootstrap: [
